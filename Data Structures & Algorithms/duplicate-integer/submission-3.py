16bd89aa0d0f63229516bd290028f453ed8f9180
Eyad Selmy <eyadselmy1@gmail.com>
2026-09-02 21:41:19 +0100

    Removed submission-3.py

--- Data Structures & Algorithms/duplicate-integer/submission-3.py
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -1,10 +1,1 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        h = {}
-        for i, num in enumerate(nums):
-            h[num] = i
 
-        for i, num in enumerate(nums):
-            if num in h and i != h[num]:
-                return True
-        return False
